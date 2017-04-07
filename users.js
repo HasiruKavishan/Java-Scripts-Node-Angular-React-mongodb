84c3d81f649ce9ef49e815df5ab555d2159fea29
HasiruKavishan <bgh.kavishan@gmail.com>
2017-04-07 23:50:42 +0530

    Add files via upload

--- users.js
+++ users.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/users",function(req,res){
+router.get("",function(req,res){
     res.send("hello world users");
 });
 
