070e8604e0d68c9928470d5e6872949360799052
HasiruKavishan <bgh.kavishan@gmail.com>
2017-04-07 23:39:11 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hasiru on 4/6/17.
+ */
+
+const express = require('express');
+const router = express.Router();
+
+router.get("/users",function(req,res){
+    res.send("hello world users");
+});
+
+module.exports = router;
